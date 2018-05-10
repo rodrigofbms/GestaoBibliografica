75aa15626ff7d6a8ddca917857c27e9d214d242c
Rodrigo <rodrigofbms@hotmail.com>
2018-05-09 21:12:22 -0300

    Adicionando Banco a aplicação

---
+++ src/br/ucsal/entidades/Usuario.java
@@ -0,0 +1,35 @@
+package br.ucsal.entidades;
+
+import java.util.ArrayList;
+
+public class Usuario {
+    private Integer id;
+    private String login;
+    private String senha;
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+
+}
