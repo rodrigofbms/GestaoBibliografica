5e2e500e03d104b09f90c64464b74cfb91d7ec94
Rodrigo <rodrigofbms@hotmail.com>
2018-06-11 19:52:52 -0300

    atualizando as denominações do banco

--- src/br/ucsal/dao/BibliografiaDAO.java
+++ src/br/ucsal/dao/BibliografiaDAO.java
@@ -49,28 +49,26 @@ public class BibliografiaDAO {
         return bibliografias;
     }
 
-    // A PROCURA DE UMA UTILIDADE DESSE METODO
+// PROCURANDO UMA UTILIDADE
     public List<Bibliografia> listar() {
         Statement stmt;
         List<Bibliografia> Bibliografias = new ArrayList<>();
         try {
             stmt = conexao.getConnection().createStatement();
-            ResultSet rs = stmt.executeQuery("select BibliografiaS.ID, Titulo, ID_AUTOR, EDICAO, EDITORA;");
+            ResultSet rs = stmt.executeQuery("select BibliografiaS.ID, Titulo, ID_AUTOR, EDICAO, EDITORA from bibliografia;");
             while (rs.next()) {
-                Bibliografia l = new Bibliografia();
-                l.setId(rs.getInt("id"));
-                l.setTitulo(rs.getString("Titulo"));
-                l.setEdicao(rs.getInt("edicao"));
-                l.setEditora(rs.getString("editora"));
+                Bibliografia bibliografia = new Bibliografia();
+                bibliografia.setId(rs.getInt("id"));
+                bibliografia.setTitulo(rs.getString("Titulo"));
+                bibliografia.setEdicao(rs.getInt("edicao"));
+                bibliografia.setEditora(rs.getString("editora"));
 
                 Autor autor = new Autor();
-                autor.setId(rs.getInt("autor_id"));
-                autor.setNome(rs.getString("Nome"));
-                autor.setSobrenome(rs.getString("Sobrenome"));
+                autor.setId(rs.getInt("id_autor"));
+                bibliografia.setAutor(autor);
 
-                l.setAutor(autor);
 
-                Bibliografias.add(l);
+                Bibliografias.add(bibliografia);
             }
             stmt.close();
         } catch (SQLException e) {
